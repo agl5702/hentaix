fc9bded9a1250129c275df97c0606ea247998140
agl5702 <advasquez41@misena.edu.co>
2024-06-28 19:30:59 -0500

    anuncioo

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ function App() {
   return (
     <>
       <h1>HentaiX</h1>
+      <a
+        href="https://www.highrevenuenetwork.com/qqfpq6n3pi?key=ef9236e819e90a08c2193973b410a87c"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Click aquí para más información
+      </a>
     </>
   );
 }
