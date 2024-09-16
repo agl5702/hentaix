dfd8785a4c4aa29974642bfefda35916488a8f0f
agl5702 <advasquez41@misena.edu.co>
2024-09-15 21:41:18 -0500

    add

--- src/api/api.api.js
+++ src/api/api.api.js
@@ -1,7 +1,20 @@
 import axios from "axios";
 
+// Crear una instancia de axios con la URL de la guía de mango
 const baseURL = axios.create({
-  baseURL: "https://tuhentai.onrender.com/animes",
+  baseURL: "https://guia-mango.onrender.com/",
 });
 
-export const getAllAnimes = () => baseURL.get();
+// Función que realiza la petición GET
+export const getAllAnimes = () =>
+  baseURL
+    .get()
+    .then((response) => {
+      console.log(response.data); // Manejar la respuesta
+    })
+    .catch((error) => {
+      console.error("Error al realizar la petición:", error);
+    });
+
+// Llamar la función cada 10 minutos (600,000 milisegundos)
+setInterval(getAllAnimes, 600000); // 10 minutos = 600000 ms
